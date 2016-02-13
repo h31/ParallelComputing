1dce31bf5b95bc9cbef645fdef7214084db678e5
Artyom Aleksyuk <artyom.h31@gmail.com>
2016-02-14 01:31:01 +0300

    Position check is not needed anymore

--- utils.h
+++ utils.h
@@ -34,15 +34,11 @@ std::multimap<B,A> flipMap(const std::map<A,B> &src)
 
 void printStats(wordStatFlipped stat) {
     printf("Stats: \n");
-    int position = 0;
 
     const unsigned long showFirst = 5;
     auto end = stat.size() > showFirst ? std::next(stat.rbegin(), showFirst) : stat.rend();
 
     for (auto it = stat.rbegin(); it != end; ++it) {
-        if (++position > 5) {
-            break;
-        }
         printf("%5d %s \n", it->first, it->second.c_str());
     }
 }
@@ -64,7 +60,7 @@ char* loadTextFromFile(const char* path) {
     memset(buffer, '\0', (size_t) lSize);
 
     // copy the file into the buffer:
-    int result = fread(buffer, 1, lSize, file);
+    fread(buffer, 1, lSize, file);
 
     return buffer;
 }
